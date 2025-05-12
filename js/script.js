56af27e51bff3a58425cac308950d39d3178f760
redhatwitch <juliaroksi28@gmail.com>
2025-05-12 21:59:53 +0800

    fitur: add shopping cart fitur

--- js/script.js
+++ js/script.js
@@ -38,3 +38,22 @@ document.addEventListener("click", function (e) {
     searchForm.classList.remove("active");
   }
 });
+
+// SHOPPING CART
+// menyeleksi shopping-cart
+const shopCart = document.querySelector(".shopping-cart");
+
+// mengklik hamburger menu
+document.querySelector("#shopping-cart").onclick = (e) => {
+  shopCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// mengklik di halaman untuk menutup sidebar
+const shopBtn = document.querySelector("#shopping-cart");
+
+document.addEventListener("click", function (e) {
+  if (!shopBtn.contains(e.target) && !shopCart.contains(e.target)) {
+    shopCart.classList.remove("active");
+  }
+});
