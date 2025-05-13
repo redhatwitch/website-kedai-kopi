c9227ab67a4468ab632f07716cadd8cb9bcb0080
redhatwitch <juliaroksi28@gmail.com>
2025-05-13 22:15:43 +0800

    fitur: add modal box for product detail

--- js/script.js
+++ js/script.js
@@ -57,3 +57,27 @@ document.addEventListener("click", function (e) {
     shopCart.classList.remove("active");
   }
 });
+
+// MODAL BOX
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
